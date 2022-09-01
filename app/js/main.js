dcdd70c1382e9dfb6de28406e73dddaa7fccabb3
Valerii Stolbov <stolbov.v@outlook.com>
2022-09-01 20:10:15 +0500

    work on styles

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,34 @@
 /* global */
 window.addEventListener('DOMContentLoaded', () => {
-  // Audio wawes
+  // Header-menu
+  const headerMenu = document.querySelector('.header-menu');
+
+  if (headerMenu) {
+    const headerMenuButton = headerMenu.querySelector('.header-menu__button');
+    const headerMenuNav = headerMenu.querySelector('.header-menu__nav');
+
+    headerMenuButton.addEventListener('click', () => {
+      if (headerMenuNav.classList.contains('--active')) {
+        headerMenuNav.classList.remove('--active');
+      } else {
+        headerMenuNav.classList.add('--active');
+      }
+    });
+
+    headerMenuNav.addEventListener('click', (e) => {
+      e.preventDefault();
+
+      const target = e.target;
+
+      if (target && target.classList.contains('header-menu__nav-link')) {
+        if (target.nextElementSibling.classList.contains('header-menu__subnav')) {
+          if (target.nextElementSibling.classList.contains('--active')) {
+            target.nextElementSibling.classList.remove('--active');
+          } else {
+            target.nextElementSibling.classList.add('--active');
+          }
+        }
+      }
+    });
+  }
 });

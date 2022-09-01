b46872cc86209083d50ba05cd8e673febb174ba3
Valerii Stolbov <stolbov.v@outlook.com>
2022-09-01 20:10:40 +0500

    Updates

--- js/main.js
+++ js/main.js
@@ -1,5 +1,33 @@
 "use strict";
 
 /* global */
-window.addEventListener('DOMContentLoaded', function() { // Audio wawes
+window.addEventListener('DOMContentLoaded', function() {
+  // Header-menu
+  var headerMenu = document.querySelector('.header-menu');
+
+  if (headerMenu) {
+    var headerMenuButton = headerMenu.querySelector('.header-menu__button');
+    var headerMenuNav = headerMenu.querySelector('.header-menu__nav');
+    headerMenuButton.addEventListener('click', function() {
+      if (headerMenuNav.classList.contains('--active')) {
+        headerMenuNav.classList.remove('--active');
+      } else {
+        headerMenuNav.classList.add('--active');
+      }
+    });
+    headerMenuNav.addEventListener('click', function(e) {
+      e.preventDefault();
+      var target = e.target;
+
+      if (target && target.classList.contains('header-menu__nav-link')) {
+        if (target.nextElementSibling.classList.contains('header-menu__subnav')) {
+          if (target.nextElementSibling.classList.contains('--active')) {
+            target.nextElementSibling.classList.remove('--active');
+          } else {
+            target.nextElementSibling.classList.add('--active');
+          }
+        }
+      }
+    });
+  }
 });

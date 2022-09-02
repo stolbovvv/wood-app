2fdbf194aba2449a15bfc48589d216cd8d17a8c7
Valerii Stolbov <stolbov.v@outlook.com>
2022-09-02 19:07:05 +0500

    Updates

--- js/main.js
+++ js/main.js
@@ -1,33 +1,86 @@
 "use strict";
 
-/* global */
+/* global gsap, Swiper */
 window.addEventListener('DOMContentLoaded', function() {
   // Header-menu
-  var headerMenu = document.querySelector('.header-menu');
+  var headerMenuButton = document.querySelector('.header-menu__button');
+  var headerMenuBody = document.querySelector('.header-menu__body');
+  var headerMenuList = document.querySelector('.header-menu__list');
+  headerMenuButton.addEventListener('click', function() {
+    if (headerMenuButton.classList.contains('--active')) {
+      headerMenuButton.classList.remove('--active');
+      document.body.classList.remove('--lock');
+      gsap.to(headerMenuBody, {
+        duration: 0.5,
+        y: 0
+      });
+    } else {
+      headerMenuButton.classList.add('--active');
+      document.body.classList.add('--lock');
+      gsap.to(headerMenuBody, {
+        duration: 0.5,
+        y: '100%'
+      });
+    }
+  });
+  headerMenuList.addEventListener('click', function(e) {
+    var documentWidth = document.documentElement.clientWidth;
 
-  if (headerMenu) {
-    var headerMenuButton = headerMenu.querySelector('.header-menu__button');
-    var headerMenuNav = headerMenu.querySelector('.header-menu__nav');
-    headerMenuButton.addEventListener('click', function() {
-      if (headerMenuNav.classList.contains('--active')) {
-        headerMenuNav.classList.remove('--active');
-      } else {
-        headerMenuNav.classList.add('--active');
-      }
-    });
-    headerMenuNav.addEventListener('click', function(e) {
+    if (e.target.classList.contains('header-menu__link') && documentWidth < 1280) {
       e.preventDefault();
-      var target = e.target;
 
-      if (target && target.classList.contains('header-menu__nav-link')) {
-        if (target.nextElementSibling.classList.contains('header-menu__subnav')) {
-          if (target.nextElementSibling.classList.contains('--active')) {
-            target.nextElementSibling.classList.remove('--active');
-          } else {
-            target.nextElementSibling.classList.add('--active');
-          }
+      if (e.target.classList.contains('--active')) {
+        e.target.classList.remove('--active');
+
+        if (e.target.nextElementSibling.classList.contains('header-menu__submenu')) {
+          gsap.to(e.target.nextElementSibling, {
+            duration: 0.5,
+            height: 0
+          });
+        }
+      } else {
+        e.target.classList.add('--active');
+
+        if (e.target.nextElementSibling.classList.contains('header-menu__submenu')) {
+          gsap.to(e.target.nextElementSibling, {
+            duration: 0.5,
+            height: 'auto'
+          });
         }
       }
-    });
-  }
+    }
+  }); // sliders
+
+  var articlesSlider = document.querySelector('.articles-slider__slider');
+  new Swiper(articlesSlider, {
+    // Default parameters
+    slidesPerView: 1,
+    spaceBetween: 92,
+    loop: true,
+    navigation: {
+      prevEl: '.slider-button-group__button-prev',
+      nextEl: '.slider-button-group__button-next'
+    },
+    // Responsive breakpoints
+    breakpoints: {
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 15
+      },
+      1280: {
+        slidesPerView: 2,
+        spaceBetween: 32
+      },
+      1920: {
+        slidesPerView: 2,
+        spaceBetween: 92
+      }
+    }
+  }); // video
+
+  var videoContent = document.querySelector('#video-new-poject');
+  var videoButton = document.querySelector('#video-button-play');
+  videoButton.addEventListener('click', function() {
+    videoContent.play();
+  });
 });

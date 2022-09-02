8603303af6b8f612bbbd5de8eb9490ff00ac6099
Valerii Stolbov <stolbov.v@outlook.com>
2022-09-02 20:25:23 +0500

    Updates

--- js/main.js
+++ js/main.js
@@ -1,6 +1,43 @@
 "use strict";
 
-/* global gsap, Swiper */
+function ownKeys(object, enumerableOnly) {
+  var keys = Object.keys(object);
+  if (Object.getOwnPropertySymbols) {
+    var symbols = Object.getOwnPropertySymbols(object);
+    enumerableOnly && (symbols = symbols.filter(function(sym) {
+      return Object.getOwnPropertyDescriptor(object, sym).enumerable;
+    })), keys.push.apply(keys, symbols);
+  }
+  return keys;
+}
+
+function _objectSpread(target) {
+  for (var i = 1; i < arguments.length; i++) {
+    var source = null != arguments[i] ? arguments[i] : {};
+    i % 2 ? ownKeys(Object(source), !0).forEach(function(key) {
+      _defineProperty(target, key, source[key]);
+    }) : Object.getOwnPropertyDescriptors ? Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)) : ownKeys(Object(source)).forEach(function(key) {
+      Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key));
+    });
+  }
+  return target;
+}
+
+function _defineProperty(obj, key, value) {
+  if (key in obj) {
+    Object.defineProperty(obj, key, {
+      value: value,
+      enumerable: true,
+      configurable: true,
+      writable: true
+    });
+  } else {
+    obj[key] = value;
+  }
+  return obj;
+}
+
+/* global gsap, Swiper, WaveSurfer */
 window.addEventListener('DOMContentLoaded', function() {
   // Header-menu
   var headerMenuButton = document.querySelector('.header-menu__button');
@@ -78,9 +115,75 @@ window.addEventListener('DOMContentLoaded', function() {
     }
   }); // video
 
-  var videoContent = document.querySelector('#video-new-poject');
-  var videoButton = document.querySelector('#video-button-play');
+  var videoContent = document.querySelector('#video_new_poject');
+  var videoButton = document.querySelector('#video_button_play');
   videoButton.addEventListener('click', function() {
     videoContent.play();
+  }); // Audio wawes
+
+  var playerBytton = document.querySelector('#audio_player_button');
+  var audioBad = document.querySelector('#wave_audio_bad').getAttribute('data-sound');
+  var audioGreat = document.querySelector('#wave_audio_great').getAttribute('data-sound');
+  var audioToggler = document.querySelector('.difference__switch-toggler');
+  var audioLabels = document.querySelectorAll('.difference__switch-label');
+  var wavesSetting = {
+    waveColor: '#CACACA',
+    progressColor: '#787d46',
+    cursorColor: 'transparent',
+    height: 100,
+    barHeight: 5,
+    barMinHeight: 4,
+    barWidth: 4,
+    barRadius: 2,
+    barGap: 3
+  };
+  var wavesAudioBad = WaveSurfer.create(_objectSpread({
+    container: '#wave_audio_bad'
+  }, wavesSetting));
+  var wavesAudioGreat = WaveSurfer.create(_objectSpread({
+    container: '#wave_audio_great'
+  }, wavesSetting));
+  wavesAudioBad.load(audioBad);
+  wavesAudioGreat.load(audioGreat);
+
+  function muteAudio() {
+    if (audioToggler.getAttribute('data-state') === 'not-active') {
+      wavesAudioBad.setVolume(1);
+      wavesAudioGreat.setVolume(0);
+    } else {
+      wavesAudioBad.setVolume(0);
+      wavesAudioGreat.setVolume(1);
+    }
+  }
+
+  playerBytton.addEventListener('click', function() {
+    muteAudio();
+
+    if (playerBytton.getAttribute('data-state') === 'pause') {
+      playerBytton.setAttribute('data-state', 'play');
+      wavesAudioBad.play();
+      wavesAudioGreat.play();
+    } else {
+      playerBytton.setAttribute('data-state', 'pause');
+      wavesAudioBad.pause();
+      wavesAudioGreat.pause();
+    }
+  });
+  audioToggler.addEventListener('click', function() {
+    if (audioToggler.getAttribute('data-state') === 'not-active') {
+      audioToggler.setAttribute('data-state', 'active');
+      audioLabels.forEach(function(label) {
+        if (label.id === 'labe_audio_great') label.setAttribute('data-state', 'active');
+        if (label.id === 'labe_audio_bad') label.setAttribute('data-state', 'not-active');
+      });
+    } else {
+      audioToggler.setAttribute('data-state', 'not-active');
+      audioLabels.forEach(function(label) {
+        if (label.id === 'labe_audio_great') label.setAttribute('data-state', 'note-active');
+        if (label.id === 'labe_audio_bad') label.setAttribute('data-state', 'active');
+      });
+    }
+
+    muteAudio();
   });
 });

75e75bf969a57cc9b0d375ed2db93ba81b1a031c
Valerii Stolbov <stolbov.v@outlook.com>
2022-12-09 16:30:23 +0500

    fix new modal

--- app/js/main.js
+++ app/js/main.js
@@ -902,6 +902,8 @@ window.addEventListener('DOMContentLoaded', () => {
     const modalImgContainer = modalSamples.querySelector('.modal-samples__preview-wrapper-img');
     const modalListSamples = modalSamples.querySelectorAll('.samples-card__input');
 
+    modalSamples.style.height = window.innerHeight;
+
     window.addEventListener('resize', () => {
       modalSamples.style.height = window.innerHeight;
     });
